51ff13af64ac6dbc2b4eee1700a6aa9397a65a83
DongJun Shin <wq0212@naver.com>
2021-12-05 02:08:44 +0900

    dfs

--- main.js
+++ main.js
@@ -209,7 +209,7 @@ app.get('/faq-down/:id', (req, res) => {
 app.get('/search', (req, res) => {
     var query = url.parse(req.url, true).query.query;
     const sql_call_num = "SELECT c_id,c_keyword,c_num,c_where FROM tb_call WHERE (c_keyword LIKE ? OR c_where LIKE ?) ORDER BY c_id DESC ";
-    const sql_qna = "SELECT q_id,q_question,q_date,q_hit,q_ans_cnt FROM tb_qna WHERE (NOT q_ans_cnt=0) AND (q_question LIKE ? OR q_content LIKE ?) ORDER BY q_id DESC";
+    const sql_qna = "SELECT q_id,q_question,q_date,q_hit,q_ans_cnt FROM tb_qna WHERE ((NOT q_ans_cnt=0) AND ((q_question LIKE ?) OR (q_content LIKE ?))) ORDER BY q_id DESC";
     const sql_faq = "SELECT f_id,f_question,f_date,f_hit FROM tb_faq WHERE (f_question LIKE ? OR f_answer LIKE ? OR f_tag LIKE ?) ORDER BY f_id DESC";
 
     const query_ ='%'+ Buffer.from(query, "utf8").toString('base64')+'%';
